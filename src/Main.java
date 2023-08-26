1c9e5530928dd5051a721be807dea2902fa8e1ee
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-08-26 16:09:21 +0800

    > make 20230002 dezi Linux dezi 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:09:21 up 26 min,  1 user,  load average: 0.38, 0.28, 0.18 9a1a28f0f226899c7be936da9d9a1d115b8e054

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
+/* lab0 实现一个简易的cat命令 */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main
 {    
-    public static void main(String[] args){}
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Usage:\n\tjava Main file_path -> miss file_path");
+        }
+
+        String file_name = args[0];
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file_name))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException exception) {
+            System.out.println("Error reading: " + exception.getMessage());
+        }
+    }
 }
