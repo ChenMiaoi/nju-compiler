2f3db7348d5203fe4594efcba5c814f8a858a31e
chenmiaoi <3026066798@qq.com>
2023-08-28 15:04:52 +0800

    fix the repo

--- submit.sh
+++ submit.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Change USERNAME, PASSWORD
-USERNAME="20230002"
+USERNAME="ky20230002"
 PASSWORD="jump-U4rgV"
 
 echo "--------Begin to Submit--------"
 
 if [[ $USERNAME == "20xxxxxxx" || $PASSWORD == "xxxxxxxxx" ]]; then
-  echo "Fail. You need to change USERNAME and PASSWORD"
-  exit -1
+	echo "Fail. You need to change USERNAME and PASSWORD"
+	exit -1
 fi
 
 COURSE=PTC2022
@@ -18,10 +18,10 @@ WORKSPACE=$(basename $(realpath .))
 FILE=submit.zip
 
 if [[ $(git status --porcelain) ]]; then
-  echo "Error: Git repository is dirty."
-  echo "Commit all your changes before submitting."
-  echo "Hint: run 'git status' to show changed files."
-  exit -1
+	echo "Error: Git repository is dirty."
+	echo "Commit all your changes before submitting."
+	echo "Hint: run 'git status' to show changed files."
+	exit -1
 fi
 
 # Construct assignmentId
@@ -34,19 +34,17 @@ ID=$(echo $ANTLRTMP | sed "s/N/${NUMBER}/g")
 echo "In branch: $BRANCH "
 echo "Submit to assignment: $ID"
 
-
 # Compress the whole folder instead of git storage only.
-cd .. 
+cd ..
 rm -f $FILE
-zip -r "$FILE" $(ls -d "$WORKSPACE/.git" 2>/dev/null) > /dev/null
+zip -r "$FILE" $(ls -d "$WORKSPACE/.git" 2>/dev/null) >/dev/null
 if [ $? -ne 0 ]; then
-  echo ""
-  echo "Fail to zip for submit.zip!"
+	echo ""
+	echo "Fail to zip for submit.zip!"
 else
-  echo ""
-  echo "generate submit.zip"
+	echo ""
+	echo "generate submit.zip"
 fi
-
 
 # construct json for cul
 TMPRAW='{"username":"XXXUSERXXX","password":"YYYPWDYYY"}'
@@ -55,24 +53,23 @@ URL=http://back.compilers.cpl.icu
 
 # extract token
 RAW=$(curl "$URL/auth/login" \
-  -H 'Content-Type: application/json' \
-  --data-raw $DATARAW )
+	-H 'Content-Type: application/json' \
+	--data-raw $DATARAW)
 TOKEN=$(echo $RAW | sed 's/,/\n/g' | grep "token" | sed 's/:/\n/g' | sed '1d' | sed 's/}//g' | sed 's/\"//g')
 
 RES=$(curl "$URL/submissions" \
-  -H "Authorization: Bearer $TOKEN" \
-  -F "assignmentId=$ID" \
-  -F "file=@$FILE" )
+	-H "Authorization: Bearer $TOKEN" \
+	-F "assignmentId=$ID" \
+	-F "file=@$FILE")
 
 FLAG0=$?
 
 TIME=$(echo $RES | sed 's/,/\n/g' | grep "createdAt" | sed 's/:/\n/g' | sed '1d' | sed 's/}//g' | sed 's/\"//g')
 
 if [[ $FLAG0 -ne 0 || -z $TIME ]]; then
-  echo ""
-  echo "T_T Commit Fail! T_T"
+	echo ""
+	echo "T_T Commit Fail! T_T"
 else
-  echo ""
-  echo "^v^ Commit Success! ^v^ "
+	echo ""
+	echo "^v^ Commit Success! ^v^ "
 fi
-
