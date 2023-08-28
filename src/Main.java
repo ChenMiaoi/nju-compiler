b61c4ae25fe0a883ab971fa894511ffe2379f638
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-08-28 11:34:14 +0800

    > make 20230002 dezi Linux dezi 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:34:14 up 37 min,  1 user,  load average: 0.42, 0.17, 0.11 2e0e6ee743092b75449c4f00306e341b3a9195e

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 
@@ -17,10 +18,15 @@ public class Main
         SysYLexer sysYLexer = new SysYLexer(inputStream);
 
         sysYLexer.removeErrorListeners();
+        sysYLexer.addErrorListener(SysYErrorListener());
         List<? extends Token> tokens = sysYLexer.getAllTokens();
 
         for (Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    private static ANTLRErrorListener SysYErrorListener() {
+        return null;
     }
 }
