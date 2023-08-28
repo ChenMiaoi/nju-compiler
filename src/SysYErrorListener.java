718a8734b3184f99b43cc3a9b634b28806fe3821
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-08-28 11:45:01 +0800

    > make 20230002 dezi Linux dezi 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:45:01 up 47 min,  1 user,  load average: 0.04, 0.07, 0.08 f5772201e3db6689ae69b577cad2368146e0d509

--- src/SysYErrorListener.java
+++ src/SysYErrorListener.java
@@ -3,6 +3,6 @@ import org.antlr.v4.runtime.*;
 public class SysYErrorListener extends BaseErrorListener {
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-        System.err.println(offendingSymbol + " at Line " + line);
+        System.err.println("Error type A at Line" + line + ": Mysterious character \"" + offendingSymbol + "\"");
     }
 }
