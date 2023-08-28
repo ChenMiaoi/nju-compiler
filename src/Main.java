f304efc73a02a65c7996813637a30e2d64f5ab68
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-08-28 12:04:06 +0800

    > make 20230002 dezi Linux dezi 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:04:06 up  1:07,  1 user,  load average: 0.73, 0.24, 0.12 57e6d3ecb15186794e47384bc71e4385f6a8e1cf

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,14 @@ import org.antlr.v4.runtime.CharStreams;
 
 public class Main
 {    
+    private static String[] tokenSymbols = {
+			null, "CONST", "INT", "VOID", "IF", "ELSE", "WHILE", "BREAK", "CONTINUE", 
+			"RETURN", "PLUS", "MINUS", "MUL", "DIV", "MOD", "ASSIGN", "EQ", "NEQ", 
+			"LT", "GT", "LE", "GE", "NOT", "AND", "OR", "L_PAREN", "R_PAREN", "L_BRACE", 
+			"R_BRACE", "L_BRACKT", "R_BRACKT", "COMMA", "SEMICOLON", "IDENT", "INTEGER_CONST", 
+			"WS", "LINE_COMMENT", "MULTILINE_COMMENT"
+		};
+
     public static void main(String[] args) throws IOException {
         if (args.length < 1) {
             System.err.println("input path is required!");
@@ -21,12 +29,11 @@ public class Main
         sysYLexer.removeErrorListeners();
         SysYErrorListener sysYErrorListener = new SysYErrorListener();
         sysYLexer.addErrorListener(sysYErrorListener);
-        System.out.println(sysYLexer.getTokenNames()[1]);
+
         // 获取自定义后的token输出
         List<? extends Token> tokens = sysYLexer.getAllTokens();
         for (Token token : tokens) {
-
-            System.out.println(token.getType() + " " + token.getText() + " at Line " + token.getLine());
+            System.out.println(tokenSymbols[token.getType()] + " " + token.getText() + " at Line " + token.getLine());
         }
     }
 }
--- src/SysYErrorListener.java
+++ src/SysYErrorListener.java
@@ -3,7 +3,7 @@ import org.antlr.v4.runtime.*;
 public class SysYErrorListener extends BaseErrorListener {
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-        String simbol = msg.substring(msg.indexOf("'") + 1, msg.lastIndexOf("'"));
-        System.err.println("Error type A at Line " + line + ": Mysterious character \"" + simbol + "\".");
+        String symbol = msg.substring(msg.indexOf("'") + 1, msg.lastIndexOf("'"));
+        System.err.println("Error type A at Line " + line + ": Mysterious character \"" + symbol + "\".");
     }
 }
